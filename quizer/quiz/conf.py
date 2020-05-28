dedc2fbb1f186276b1ad5a75c1b3b8c4b481ffed
javad abdi <abdijavad110@gmail.com>
2020-05-28 17:11:05 +0430

    fix minor bug

--- quizer/quiz/conf.py
+++ quizer/quiz/conf.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
 
 
-START_TIME = datetime(year=2020, month=5, day=28, hour=17, minute=8, second=5)
+START_TIME = datetime(year=2020, month=5, day=28, hour=17, minute=12, second=5)
 STOP_TIME = datetime(year=2021, month=1, day=1)
