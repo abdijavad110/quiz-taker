acd6f590ea5d3453edb45c3c23f67237594b535a
javad abdi <abdijavad110@gmail.com>
2020-05-28 15:43:36 +0430

    add models to admin

--- quizer/quiz/admin.py
+++ quizer/quiz/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Question, Answer
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('type', 'name', 'text', 'pic',)
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('responder', 'file', 'time')
+
+
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- quizer/quiz/models.py
+++ quizer/quiz/models.py
@@ -7,8 +7,10 @@ class Question(models.Model):
     name = models.CharField(max_length=15, default="بی‌نام")
     text = models.TextField(default="", blank=True)
     pic = models.ImageField(upload_to="questions", default=None, blank=True)
+    type = models.IntegerField(default=0)
 
 
 class Answer(models.Model):
     file = models.FileField(upload_to="answers")
+    time = models.DateTimeField(auto_now=True)
     responder = models.ForeignKey(User, models.CASCADE)
