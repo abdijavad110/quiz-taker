2245b081722eb47a11b0ffc9957e07cee29f9891
javad abdi <abdijavad110@gmail.com>
2020-05-28 12:45:26 +0430

    add signup and login

--- quizer/quizer/urls.py
+++ quizer/quizer/urls.py
@@ -1,16 +1,17 @@
 # config/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, home
+from .views import index, home, login, signup, logout
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('quiz/', include('quiz.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', index, name='home'),
-    path('test/', home)
-] + static('/media', document_root=settings.MEDIA_ROOT)
-
+                  path('admin/', admin.site.urls),
+                  path('quiz/', include('quiz.urls')),
+                  path('accounts/', include('django.contrib.auth.urls')),
+                  path('', index, name='home'),
+                  path('test/', home),
+                  path('login/', login, name='login'),
+                  path('signup/', signup, name='signup'),
+                  path('logout/', logout, name='logout'),
+              ] + static('/media', document_root=settings.MEDIA_ROOT)
--- quizer/quizer/views.py
+++ quizer/quizer/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate, logout as auth_logout
 
 
 def index(request):
@@ -11,7 +13,54 @@ def home(request):
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    errors = []
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = User.objects.get(username=username)
+        user = authenticate(request, username=username, password=password)
+        if user:
+            auth_login(request, user)
+            return redirect('/')
+        else:
+            errors.append("نام‌کاربری یا گذرواژه صحیح نمی‌باشد.")
+    return render(request, "registration/login.html", {'errors': errors})
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    errors = []
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        password_rep = request.POST['password_rep']
+        f_name = request.POST['first_name']
+        l_name = request.POST['last_name']
+
+        if User.objects.filter(username=username).exists():
+            errors.append("نام کاریری موردنظر قبلا استفاده‌شده است. از نام کاربری دیگری استفاده کنید.")
+        if len(password) < 5:
+            errors.append("گذرواژه انتخابی شما باید حداقل از ۶ حرف یا عدد تشکیل شده‌باشد.")
+        if password != password_rep:
+            errors.append("گذرواژه‌های وارد شده با هم تطابق ندارند.")
+        if f_name == "":
+            errors.append("وارد کردن نام اجباریست.")
+        if l_name == "":
+            errors.append("وارد کردن نام‌خانوادگی اجباریست.")
+        if len(errors) == 0:
+            user = User.objects.create_user(username, password=password)
+            user.last_name = l_name
+            user.first_name = f_name
+            user.save()
+            auth_login(request, user)
+            return redirect('/')
+    return render(request, 'registration/signup.html', {'errors': errors})
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('/')
